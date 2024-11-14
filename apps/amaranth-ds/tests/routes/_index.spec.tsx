9154b675bd995869496a9451a593a37d6544c291
Richard Huf <richardhuf84@gmail.com>
2024-11-15 02:51:37 +1100

    refactor: clear up unused vars

--- apps/amaranth-ds/tests/routes/_index.spec.tsx
+++ apps/amaranth-ds/tests/routes/_index.spec.tsx
@@ -1,7 +1,3 @@
-import { createRemixStub } from '@remix-run/testing';
-import { render, screen, waitFor } from '@testing-library/react';
-import Index from '../../app/routes/_index';
-
 test('renders indes page', async () => {
   expect(true).toBeTruthy();
 });
