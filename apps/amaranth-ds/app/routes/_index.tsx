2cb0773200f50ec2241b91255f9ace990c7dff62
Richard Huf <richardhuf84@gmail.com>
2024-11-15 04:14:28 +1100

    feat: setup core package and storybook

--- apps/amaranth-ds/app/routes/_index.tsx
+++ apps/amaranth-ds/app/routes/_index.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@amaranth-ds/core';
+
 export default function Index() {
   return (
     <div>
       <img src="./amaranth-ds-logo.png" alt="Home" />
+      <Button variant="primary">Primary button</Button>
+      <Button variant="secondary">Secondary button</Button>
     </div>
   );
 }
